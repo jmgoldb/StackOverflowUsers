00b5e39407dc9b53a98bbfce7a00579bf6237f75
Joseph Goldberg <urza2016@gmail.com>
2018-04-05 15:05:29 -0500

    Initial Commit

---
+++ StackOverFlowUsers/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StackOverFlowUsers
+//
+//  Created by Joseph Goldberg on 4/5/18.
+//  Copyright © 2018 Joseph Goldberg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StackOverFlowUsers/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StackOverFlowUsers
+//
+//  Created by Joseph Goldberg on 4/5/18.
+//  Copyright © 2018 Joseph Goldberg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
